a4663348b89a246da8c4920f41edb824c9be71fd
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-25 22:22:45 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,59 @@
+#look at notes on below methods in sublime text green_grocer file
+
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {} 
+  cart.each do |array_element| 
+    array_element.each do |item_name, item_attributes_hash| 
+        if cart_hash[item_name] 
+          cart_hash[item_name][:count] += 1 
+        else
+          cart_hash[item_name] = item_attributes_hash 
+          cart_hash[item_name][:count] = 1  
+        end 
+    end
+  end
+  p cart_hash 
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon_hash| 
+    coupon_hash.each do |attribute, value| 
+      coupon_item_name = coupon_hash[:item] 
+      if cart[coupon_item_name] && cart[coupon_item_name][:count] >= coupon_hash[:num]  
+        if cart["#{coupon_item_name} W/COUPON"] 
+          cart["#{coupon_item_name} W/COUPON"][:count] += coupon_hash[:num] 
+        else 
+          cart["#{coupon_item_name} W/COUPON"] = { 
+            :price => coupon_hash[:cost] / coupon_hash[:num], 
+            :clearance => cart[coupon_item_name][:clearance], 
+            :count => coupon_hash[:num] 
+          }
+        end
+        cart[coupon_item_name][:count] -= coupon_hash[:num] 
+      end
+    end
+  end
+  p cart 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item_name, attribute_hash|
+      if attribute_hash[:clearance] == true 
+        attribute_hash[:price] = (attribute_hash[:price] * 0.80).round(2) 
+      end
+    end 
+  p cart 
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0 
+  cart1 = consolidate_cart(cart)
+  cart2 = apply_coupons(cart1, coupons)
+  cart3 = apply_clearance(cart2)
+  
+  cart3.each do |item_name, attribute_hash|
+    total = total + (attribute_hash[:price] * attribute_hash[:count])
+  end
+  
+  p cart
 end
