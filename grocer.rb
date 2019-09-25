dddba8b185e1322284b5d5658779499de108ab36
Bailey Wood <woodbmae@gmail.com>
2019-09-25 14:03:59 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,26 @@
+
+cart = [
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+
+
 def consolidate_cart(cart)
-  # code here
+  cart = {}
+  cart.each do |item|
+    item_name = item.keys[0]
+    if output[item name]
+      output[item_name][:count] += 1
+    else
+      output[item_name] = item[item_name]
+      output[item_name][:count] = 1 
+    end
+  end
+  output
 end
+
+
 
 def apply_coupons(cart, coupons)
   # code here
@@ -13,3 +33,27 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+describe "Grocer" do
+  let(:items) do
+    [
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"KALE" => {:price => 3.00, :clearance => false}},
+      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+      {"ALMONDS" => {:price => 9.00, :clearance => false}},
+      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+      {"CHEESE" => {:price => 6.50, :clearance => false}},
+      {"BEER" => {:price => 13.00, :clearance => false}},
+      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]
+  end
+
+  let(:coupons) do
+    [
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "BEER", :num => 2, :cost => 20.00},
+      {:item => "CHEESE", :num => 3, :cost => 15.00}
+    ]
+  end
