a9a3dbeff39fa81eef71fd1390b7378cc68b524d
Bailey Wood <woodbmae@gmail.com>
2019-10-04 16:33:24 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,59 +1,60 @@
 
-cart = [
-  {"AVOCADO" => {:price => 3.00, :clearance => true }},
-  {"AVOCADO" => {:price => 3.00, :clearance => true }},
-  {"KALE"    => {:price => 3.00, :clearance => false}}
-]
-
 
 def consolidate_cart(cart)
-  cart = {}
-  cart.each do |item|
-    item_name = item.keys[0]
-    if output[item name]
-      output[item_name][:count] += 1
+  final_hash = {}
+  cart.each do |element_hash| #cart is an array of hashes, this gets us to first hash TEMPEH
+    element_name = element_hash.keys[0]    # .keys hash method returns an array with all the keys -> in this case "TEMPEH"
+    element_stats = element_hash.values[0] # .values hash method returns an array with all the values -> in this case price/clearance hash of "TEMPEH"
+    if final_hash.has_key?(element_name)
+      final_hash[element_name][:count] += 1 
     else
-      output[item_name] = item[item_name]
-      output[item_name][:count] = 1 
+      final_hash[element_name] = {
+        count: 1,
+        price: element_stats[:price],
+        clearance: element_stats[:clearance]
+      }
     end
   end
-  output
+  final_hash
 end
-
-
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    item = coupon[:item]
+    coupon_item = "#{item} W/COUPON"
+      if cart[item]
+        if !cart[coupon_item] && cart[item][:count] >= coupon[:num]
+          cart[coupon_item] = {
+            price: coupon[:cost] / coupon[:num],
+            clearance: cart[item][:clearance],
+            count: coupon[:num]
+          }
+          cart[item][:count] -= coupon[:num]
+        elsif cart[coupon_item] && cart[item][:count] >= coupon[:num]
+          cart[coupon_item][:count] += coupon[:num]
+          cart[item][:count] -= coupon[:num]
+        end
+      end
+    end
+  cart
 end
+
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |product_name, stats|
+    stats[:price] -= stats[:price] * 0.2 if stats[:clearance] == true
+  end
+  cart
 end
 
-def checkout(cart, coupons)
-  # code here
+
+def checkout(array, coupons)
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_clearance = apply_clearance(applied_coupons)
+  total = applied_clearance.reduce(0) do |current_sum, (k, v)| 
+    current_sum += v[:price] * v[:count] 
+  end
+  total > 100 ?  total * 0.9 : total
 end
 
-describe "Grocer" do
-  let(:items) do
-    [
-      {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"KALE" => {:price => 3.00, :clearance => false}},
-      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-      {"ALMONDS" => {:price => 9.00, :clearance => false}},
-      {"TEMPEH" => {:price => 3.00, :clearance => true}},
-      {"CHEESE" => {:price => 6.50, :clearance => false}},
-      {"BEER" => {:price => 13.00, :clearance => false}},
-      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-      {"BEETS" => {:price => 2.50, :clearance => false}},
-      {"SOY MILK" => {:price => 4.50, :clearance => true}}
-    ]
-  end
-
-  let(:coupons) do
-    [
-      {:item => "AVOCADO", :num => 2, :cost => 5.00},
-      {:item => "BEER", :num => 2, :cost => 20.00},
-      {:item => "CHEESE", :num => 3, :cost => 15.00}
-    ]
-  end
